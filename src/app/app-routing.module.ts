c091927b4d1e61c24a58d253502bbc23b48ce1b9
matiasisaacdavila22 <mipcomputacion@gmail.com>
2021-06-17 11:56:40 -0300

    add router de maste-page in routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DefaultComponent } from './public/home/default/default.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component:DefaultComponent
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: '/home'
+  },
+
+ /**this must be the last one*/
+{
+  path:'**',
+  redirectTo: '/home'
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
