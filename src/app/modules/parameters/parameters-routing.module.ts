3f760ccb38267ce244ff611c6cc0b4f7c45a09da
matiasisaacdavila22 <mipcomputacion@gmail.com>
2021-06-18 08:25:30 -0300

    Routing for security and parameters from app.routing with sazy loading, in addiotion, materialize styles were added.

--- src/app/modules/parameters/parameters-routing.module.ts
+++ src/app/modules/parameters/parameters-routing.module.ts
@@ -1,7 +1,43 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BrandCreationComponent } from './brand/brand-creation/brand-creation.component';
+import { BrandEditionComponent } from './brand/brand-edition/brand-edition.component';
+import { BrandListComponent } from './brand/brand-list/brand-list.component';
+import { CategoryCreationComponent } from './category/category-creation/category-creation.component';
+import { CategoryEditionComponent } from './category/category-edition/category-edition.component';
+import { CategoryListComponent } from './category/category-list/category-list.component';
+import { CategoryRemoveComponent } from './category/category-remove/category-remove.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'category-list',
+    component: CategoryListComponent
+  },
+  {
+    path: 'category-creation',
+    component: CategoryCreationComponent
+  },
+  {
+    path: 'category-edition',
+    component: CategoryEditionComponent
+  },
+  {
+    path: 'category-remove',
+    component: CategoryRemoveComponent
+  },
+  {
+    path: 'brand-list',
+    component: BrandListComponent
+  },
+  {
+    path: 'brand-creation',
+    component: BrandCreationComponent
+  },
+  {
+    path: 'brand-edition',
+    component: BrandEditionComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
